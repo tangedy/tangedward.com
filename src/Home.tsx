615cb025091ce68eebcbdbf2a5800b55974a7b24
Tangedy <153305844+tangedy@users.noreply.github.com>
2026-07-30 13:14:07 -0400

    home page updated text

--- src/Home.tsx
+++ src/Home.tsx
@@ -110,7 +110,7 @@ function Home() {
                   ref={summaryText.ref}
                   className={`summary-text fade-in-element ${summaryText.isVisible ? 'visible' : ''}`}
                 >
-                  Edward is a developer who loves<br/> building design-focused digital solutions,<br/> and exploring the ways<br/> users interact with technology and AI.<br/>Previously an artist.<br/><br/>Currently a student at <br/>the University of Waterloo.
+                  Edward Tang is a developer with a love<br/> for creating software both functional <br/> and aesthetically deliberate in design. <br/> <br/> He is curious about interactive software<br/>and ways to adopt new technology <br/> seamlessly into our every day lives. <br/>Currently a student at <br/>the University of Waterloo.
                 </div>
               </div>
     
